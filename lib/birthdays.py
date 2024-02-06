9dfcfe8d2c8fc449d4320afbca9bbf6a11440bf9
Maria Jose <mj_hernandezdelprado@hotmail.com>
2024-02-06 15:35:18 +0000

    JKAHCUIAIA

--- lib/birthdays.py
+++ lib/birthdays.py
@@ -5,3 +5,11 @@ class Birthdays():
     def adds_birthday(self, name, date):
         self.birthdays[name] = date
         return self.birthdays
+    
+    def updates_date(self, name, date):
+        self.birthdays.update({name:date})
+        return self.birthdays
+    
+    def updates_name(self, old_name, name):
+        self.birthdays[name] = self.birthdays.pop(old_name)
+        return self.birthdays
--- test/test_birthdays.py
+++ test/test_birthdays.py
@@ -5,3 +5,18 @@ def test_adds_birthday():
     birthdays = Birthdays()
     result = birthdays.adds_birthday('Maria', '2000/08/17')
     assert result == {'Maria': '2000/08/17'}
+
+def test_update_date():
+    birthdays = Birthdays()
+    birthdays.adds_birthday('Maria', '2000/08/17')
+    birthdays.adds_birthday('Sam', '1994/11/28')
+    result = birthdays.updates_date("Maria","2000/08/16")
+    assert result == {'Maria': '2000/08/16','Sam': '1994/11/28'}
+
+
+def test_update_name():
+    birthdays = Birthdays()
+    birthdays.adds_birthday('Maria', '2000/08/17')
+    birthdays.adds_birthday('Sam', '1994/11/28')
+    result = birthdays.updates_name("Maria","Majo")
+    assert result == {'Majo': '2000/08/17','Sam': '1994/11/28'}
