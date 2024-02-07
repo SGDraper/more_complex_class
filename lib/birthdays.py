f7a44cab9091ecc0d0d3780bd2a2e62d1fd61fad
Samuel <samuel@seedlegals.com>
2024-02-07 10:52:18 +0000

    finishes all logic and adds more tests, all passing

--- lib/birthdays.py
+++ lib/birthdays.py
@@ -5,27 +5,42 @@ class Birthdays():
         self.birthdays = {}
 
     def adds_birthday(self, name, date):
-        self.birthdays[name] = date
+        dob = datetime.strptime(date, "%Y/%m/%d")
+        self.birthdays[name] = {'birthdate': dob, 'sent': False}
         return self.birthdays
     
     def updates_date(self, name, date):
-        self.birthdays.update({name:date})
-        return self.birthdays
+        if name in self.birthdays:
+            dob = datetime.strptime(date, '%Y/%m/%d')
+            self.birthdays[name]['birthdate'] = dob
+            return self.birthdays
+        else:
+            return f'{name} not found in list'
     
     def updates_name(self, old_name, name):
-        self.birthdays[name] = self.birthdays.pop(old_name)
-        return self.birthdays
+        if old_name in self.birthdays:
+            self.birthdays[name] = self.birthdays.pop(old_name)
+            return self.birthdays
+        else:
+            return f'{old_name} not found in list'
 
     def reminder(self, month):
         reminders = {}
-        for name, date in self.birthdays.items():
-            dob = datetime.strptime(date,"%Y/%m/%d")
+        for name, value in self.birthdays.items():
+            dob = value['birthdate']
             if dob.strftime('%B') == month:
                 age = self.calculate_age(dob)
-                reminders[name] = {"birthdate":date,"age":age}
+                reminders[name] = {"birthdate": dob.strftime('%Y/%m/%d'), "age": age}
         return reminders
     
     def calculate_age(self,birthdate):
         today = datetime.today()
         age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
         return age
+
+    def mark_sent(self,name):
+        if name in self.birthdays:
+            self.birthdays[name]['sent'] = True
+            return f'Card has been sent to {name}'
+        else:
+            return f'{name} not found'
--- test/test_birthdays.py
+++ test/test_birthdays.py
@@ -4,14 +4,15 @@ from lib.birthdays import *
 def test_adds_birthday():
     birthdays = Birthdays()
     result = birthdays.adds_birthday('Maria', '2000/08/17')
-    assert result == {'Maria': '2000/08/17'}
+    assert result == {'Maria': {'birthdate': datetime(2000, 8, 17), 'sent': False}}
 
 def test_update_date():
     birthdays = Birthdays()
     birthdays.adds_birthday('Maria', '2000/08/17')
     birthdays.adds_birthday('Sam', '1994/11/28')
     result = birthdays.updates_date("Maria","2000/08/16")
-    assert result == {'Maria': '2000/08/16','Sam': '1994/11/28'}
+    assert result == {'Maria': {'birthdate': datetime(2000, 8, 16), 'sent': False},
+                      'Sam': {'birthdate': datetime(1994, 11, 28), 'sent': False}}
 
 
 def test_update_name():
@@ -19,15 +20,22 @@ def test_update_name():
     birthdays.adds_birthday('Maria', '2000/08/17')
     birthdays.adds_birthday('Sam', '1994/11/28')
     result = birthdays.updates_name("Maria","Majo")
-    assert result == {'Majo': '2000/08/17','Sam': '1994/11/28'}
+    assert result == {'Majo': {'birthdate': datetime(2000, 8, 17), 'sent': False},
+                      'Sam': {'birthdate': datetime(1994, 11, 28), 'sent': False}}
 
 def test_birthday_reminder():
     birthdays = Birthdays()
     birthdays.adds_birthday('Maria', '2000/08/17')
     birthdays.adds_birthday('Sam', '1994/11/28')
     result = birthdays.reminder('November')
-    assert result == {'Sam':{"birthdate": "1994/11/28", "age" : 29}}
+    assert result == {'Sam':{"birthdate": "1994/11/28", "age": 29}}
 
+
+def test_mark_sent():
+    birthdays = Birthdays()
+    birthdays.adds_birthday('Maria', '2000/08/17')
+    birthdays.adds_birthday('Sam', '1994/11/28')
+    assert birthdays.mark_sent('Maria') == 'Card has been sent to Maria'
 # def test_get_ages():
 #     birthdays = Birthdays()
 #     birthdays.adds_birthday('Maria', '2000/08/17')
